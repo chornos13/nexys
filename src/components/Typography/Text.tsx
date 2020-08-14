6de8363d928d253d1a82dba218018222e8ae4d5c
chornos13 <chornos13@hotmail.com>
2020-08-14 22:43:05 +0700

    update typography

---
+++ src/components/Typography/Text.tsx
@@ -0,0 +1,56 @@
+import { CSSProperties } from 'react'
+import { Typography } from 'antd'
+import cssFont from 'css/cssFont.scss'
+import cx from 'shortcuts/cx'
+import { TextProps } from 'antd/lib/typography/Text'
+import { TextAlignProperty } from 'csstype'
+
+const { Text: CurText } = Typography
+
+const mapColor = {
+  white: 'white',
+  black: 'black',
+  gray: '#677E9B',
+  red: '#EB5757',
+  green: '#27AE60',
+  primary: '#1B5292',
+}
+
+interface IText extends TextProps {
+  className: string
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  color?: 'primary' | 'white' | 'black' | 'gray' | 'red' | 'green' | any
+  bold?: boolean
+  align?: TextAlignProperty
+  style: CSSProperties
+}
+
+function Text(props: IText) {
+  const {
+    className,
+    size = 'sm',
+    color = 'gray',
+    align,
+    bold,
+    style,
+    ...cProps
+  } = props
+
+  const weight = bold ? 'bold' : 'normal'
+
+  const classFont = `font-${size}-${weight}`
+
+  return (
+    <CurText
+      className={cx(cssFont[classFont], className)}
+      style={{
+        color: mapColor[color] || color,
+        ...(align ? { textAlign: align, display: 'block' } : {}),
+        ...style,
+      }}
+      {...cProps}
+    />
+  )
+}
+
+export default Text
--- src/components/Typography/Title.tsx
+++ src/components/Typography/Title.tsx
@@ -1,14 +1,19 @@
+import { CSSProperties } from 'react'
 import { Typography } from 'antd'
 import { TitleProps } from 'antd/lib/typography/Title'
 import cssFont from 'css/cssFont.scss'
 import cx from 'shortcuts/cx'
+import { TextAlignProperty } from 'csstype'
 import cssTitle from './Title.module.scss'
 
 const { Title: CurTitle } = Typography
 
 interface CustomTitleProps extends TitleProps {
-  color?: 'default' | 'primary'
+  className: string
+  color?: 'default' | 'primary' | any
   noMargin?: boolean
+  align?: TextAlignProperty
+  style: CSSProperties
 }
 
 const mapColor = {
@@ -17,13 +22,14 @@ const mapColor = {
 }
 
 function Title(props: CustomTitleProps) {
-  const { className, noMargin, color, style, ...otherProps } = props
+  const { className, noMargin, color, style, align, ...otherProps } = props
   return (
     <CurTitle
       className={cx(cssFont.font, cssTitle.wordSpacing, className)}
       style={{
-        color: mapColor[color] || mapColor.default,
+        color: mapColor[color] || color || mapColor.default,
         ...(noMargin ? { marginBottom: 0 } : {}),
+        ...(align ? { textAlign: align } : {}),
         ...style,
       }}
       {...otherProps}
