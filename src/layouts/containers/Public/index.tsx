e2f936c67fbab8223e52a3f3004a9ec9f7d28135
chornos13 <chornos13@hotmail.com>
2021-01-20 13:37:59 +0700

    fix(layout): update layout height to match screen height

--- src/layouts/containers/Public/index.tsx
+++ src/layouts/containers/Public/index.tsx
@@ -3,19 +3,30 @@ import { ReactComponentLike } from 'prop-types'
 import Header from 'layouts/containers/Public/Header'
 import Footer from 'layouts/containers/Public/Footer'
 import Content from 'components/Content/Content'
+import { Card } from 'antd'
 
 interface IProps {
   Component: ReactComponentLike
 }
 
+const HEIGHT_HEADER = 56
+const HEIGHT_FOOTER = 110
+
 function PublicContainer(props: IProps) {
   const { Component } = props
 
   return (
-    <div style={{ marginBottom: 64 }}>
+    <div>
       <Header />
-      <Content style={{ padding: 0, minHeight: '62vh' }}>
-        <Component {...props} />
+      <Content style={{ padding: 0 }}>
+        <Card
+          bodyStyle={{ padding: 0 }}
+          style={{
+            minHeight: `calc(100vh - ${HEIGHT_HEADER + HEIGHT_FOOTER}px)`,
+          }}
+        >
+          <Component {...props} />
+        </Card>
       </Content>
       <Footer />
     </div>
