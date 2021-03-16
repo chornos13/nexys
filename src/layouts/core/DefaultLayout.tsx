df1272dd986608af859e82c3de661cb8f84f6fed
chornos13 <chornos13@hotmail.com>
2021-03-16 17:07:48 +0700

    feat: set AppProps as type argument getSiteLayout

--- src/layouts/core/DefaultLayout.tsx
+++ src/layouts/core/DefaultLayout.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-// import { Layout } from 'antd'
 import routes from 'layouts/routes'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import matchPath from 'helpers/matchPath'
+import { AppProps } from 'next/app'
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,8 +36,8 @@ export const DefaultLayoutContext = React.createContext<
   layout: undefined,
 })
 
-function getSiteLayout(appProps) {
-  const { Component, pageProps, router } = appProps
+function getSiteLayout(props: AppProps) {
+  const { Component, pageProps, router } = props
   const { route } = router
 
   for (let i = 0; i < routes.length; i += 1) {
@@ -52,7 +52,7 @@ function getSiteLayout(appProps) {
         <DefaultLayoutContext.Provider value={curRoute}>
           <WrapperReactQuery>
             {PageLayout ? (
-              <PageLayout {...appProps} layoutProps={layoutProps} />
+              <PageLayout {...props} layoutProps={layoutProps} />
             ) : (
               <Component {...pageProps} key={router.route} />
             )}
