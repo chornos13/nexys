68978c4c6dd2c95649826004d6f66d633380e636
chornos13 <chornos13@hotmail.com>
2021-04-18 01:43:07 +0700

    Revert "perf: use getStaticProps instead getServerSideProps"
    This reverts commit 738370c9

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Home from 'views/Home/Home'
 import fs from 'fs'
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const filePath = [process.cwd(), 'src/@nexys', 'examples'].join('/')
 
   const listDir = fs.readdirSync(filePath)
