6e5c65947a2e7cff62b0fbff45f040f65c0eb1e0
masb0ymas <fajriajjha@gmail.com>
2021-05-28 09:13:26 +0700

    feat: add field FDatePicker

---
+++ src/@nexys/fields/FDatePicker/FDatePicker.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { DatePicker } from 'antd'
+import { FormikContextType, useField } from 'formik'
+import useTitleAndError, {
+  UseTitleAndErrorProps,
+} from '@nexys/fields/useTitleAndError/useTitleAndError'
+import { DatePickerProps } from 'antd/lib/date-picker'
+
+export type FDatePickerProps = DatePickerProps &
+  UseTitleAndErrorProps & {
+    name: string
+    /**
+     * Formik Context
+     */
+    formik?: FormikContextType<any>
+  }
+
+function FDatePicker(props: FDatePickerProps) {
+  const [field, , helpers] = useField(props as any)
+  const [title, error] = useTitleAndError(props)
+
+  return (
+    <React.Fragment>
+      {title}
+      <DatePicker
+        {...field}
+        {...props}
+        onBlur={() => {
+          helpers.setTouched(true)
+        }}
+        onChange={(value) => {
+          helpers.setValue(value)
+        }}
+        onOk={(value) => {
+          helpers.setValue(value)
+        }}
+      />
+      {error}
+    </React.Fragment>
+  )
+}
+
+export default FDatePicker
