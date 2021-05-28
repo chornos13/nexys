283182e12dd11edab9d1f566a5fb3743505b47b7
masb0ymas <fajriajjha@gmail.com>
2021-05-28 09:14:25 +0700

    feat: add field FRangePicker

---
+++ src/@nexys/fields/FRangePicker/FRangePicker.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { DatePicker } from 'antd'
+import { FormikContextType, useField } from 'formik'
+import useTitleAndError, {
+  UseTitleAndErrorProps,
+} from '@nexys/fields/useTitleAndError/useTitleAndError'
+import { RangePickerProps } from 'antd/lib/date-picker'
+
+const { RangePicker } = DatePicker
+
+export type FRangePickerProps = RangePickerProps &
+  UseTitleAndErrorProps & {
+    name: string
+    /**
+     * Formik Context
+     */
+    formik?: FormikContextType<any>
+  }
+
+function FRangePicker(props: FRangePickerProps) {
+  const [field, , helpers] = useField(props as any)
+  const [title, error] = useTitleAndError(props)
+
+  return (
+    <React.Fragment>
+      {title}
+      <RangePicker
+        {...field}
+        {...props}
+        onBlur={() => {
+          helpers.setTouched(true)
+        }}
+        onChange={(value) => {
+          helpers.setValue(value)
+        }}
+        onOk={(value) => {
+          helpers.setValue(value)
+        }}
+      />
+      {error}
+    </React.Fragment>
+  )
+}
+
+export default FRangePicker
